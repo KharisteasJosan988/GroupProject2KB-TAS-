e2fab16ec6a0a3766b13933a8bb735c1d99871d6
Dhaniyel25 <daniel.okta.a@mail.ukrim.ac.id>
2024-06-08 20:55:20 +0700

    Daniel

--- facemask.py
+++ facemask.py
@@ -21,3 +21,25 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+    'train',
+    target_size=(150, 150),
+    batch_size=16,
+    class_mode='binary'
+)
+
+test_set = test_datagen.flow_from_directory(
+    'test',
+    target_size=(150, 150),
+    batch_size=16,
+    class_mode='binary'
+)
